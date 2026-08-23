f2bc11f23bf8436d9189d7a8fac3aeea880a7a36
adrimartin0 <adrimartinbrazuelo@gmail.com>
2026-08-23 16:59:01 +0200

    Simplao

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH ?? '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
